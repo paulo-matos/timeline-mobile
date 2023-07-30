af86d55e4bf6f5a7f62376bc5ab2775e6a53123c
Paulo Matos <contato.paulomatos@gmail.com>
2023-07-29 23:20:45 -0300

    Added new memory with image upload from galery, image preview on page. Added memories list page and logout initial button

--- app/new.tsx
+++ app/new.tsx
@@ -1,22 +1,93 @@
-import { Link } from 'expo-router'
+import { useState } from 'react'
+import { Link, useRouter } from 'expo-router'
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import {
   Switch,
   TouchableOpacity,
+  Image,
   View,
   Text,
   TextInput,
   ScrollView,
 } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import { api } from '../src/lib/api'
 
 import Icon from '@expo/vector-icons/Feather'
 import TimelineLogo from '../src/assets/timeline-logo-horizontal.svg'
-import { useState } from 'react'
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
 
+  const [content, setContent] = useState('')
   const [isPublic, setIsPublic] = useState(false)
+  const [preview, setPreview] = useState<string | null>(null)
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        uri: preview,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+
+      try {
+        const uploadResponse = await api.post('/upload', uploadFormData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        coverUrl = uploadResponse.data.fileUrl
+      } catch (err) {
+        console.log({ err })
+      }
+    }
+
+    try {
+      await api.post(
+        '/memories',
+        {
+          content,
+          isPublic,
+          coverUrl,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+
+      router.push('/memories')
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <ScrollView
@@ -27,7 +98,7 @@ export default function NewMemory() {
         <TimelineLogo />
 
         <Link href="/memories" asChild>
-          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+          <TouchableOpacity className="h-7 w-7 items-center justify-center rounded-full bg-purple-500">
             <Icon name="arrow-left" size={16} color="#FFF" />
           </TouchableOpacity>
         </Link>
@@ -48,18 +119,30 @@ export default function NewMemory() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={openImagePicker}
           className="h-32 items-center justify-center rounded-lg border border-dashed  border-gray-500 bg-black/20"
         >
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color="#FFF" />
-            <Text className="font-body text-sm text-gray-200">
-              Add a cover video or picture
-            </Text>
-          </View>
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              className="h-full w-full rounded-lg object-cover"
+              alt="image preview"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#FFF" />
+              <Text className="font-body text-sm text-gray-200">
+                Add a cover picture
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
 
         <TextInput
           multiline
+          value={content}
+          onChangeText={setContent}
+          textAlignVertical="top"
           className="p-0 font-body text-lg text-gray-50"
           placeholderTextColor="#56565a"
           placeholder="Feel free to add pictures, videos and reports about this experience
@@ -68,6 +151,7 @@ export default function NewMemory() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={handleCreateMemory}
           className="mb-4 items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
           <Text className="font-alt text-sm uppercase text-black">Save</Text>
